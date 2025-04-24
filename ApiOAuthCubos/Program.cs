64c6f0718ce8cec900159661f80a2cdbc0996ec8
Sofía <sofiavillarejo.dev@gmail.com>
2025-04-24 10:41:19 +0200

    exam end

--- ApiOAuthCubos/Program.cs
+++ ApiOAuthCubos/Program.cs
@@ -47,10 +47,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.MapScalarApiReference();
 }
 
+app.MapOpenApi();
+app.MapScalarApiReference();
 app.MapGet("/", context =>
 {
     context.Response.Redirect("/scalar");
